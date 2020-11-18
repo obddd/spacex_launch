ee4f65072434ac10888ddc871acbe3b1c61c13de
obddd <obaidulhaq20@gmail.com>
2020-11-18 18:25:12 +0500

    added spacex logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import './App.css';
+import logo from '../src/logo.png';
 import LaunchContainer from './components/Launch';
 import LaunchInfoContainer from './components/LaunchInfo';
 
 function App() {
   return (
     <div className="App">
-      <LaunchContainer/>
-      <LaunchInfoContainer/>
+      <img
+        src={logo}
+        alt="spaceX"
+        style={{ width: 300, display: 'block', margin: 'auto' }}
+      />
+      {/* <LaunchContainer />
+      <LaunchInfoContainer /> */}
     </div>
   );
 }
